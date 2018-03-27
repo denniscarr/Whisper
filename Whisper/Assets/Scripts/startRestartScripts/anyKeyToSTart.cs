007893122176d998ad9dc1642a1fe86761e141ae
Mostafa Haque <93haque@gmail.com>
2018-03-26 22:08:41 -0400

    Tutorials
    Added tutorial scene and tutorial videos

--- Whisper/Assets/Scripts/startRestartScripts/anyKeyToSTart.cs
+++ Whisper/Assets/Scripts/startRestartScripts/anyKeyToSTart.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class anyKeyToSTart : MonoBehaviour {
-
+    int currentSceneIndex;
+    int nextSceneIndex;
 	// Use this for initialization
 	void Start () {
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        nextSceneIndex = currentSceneIndex + 1;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.anyKeyDown) {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(nextSceneIndex);
         }
 	}
 }
